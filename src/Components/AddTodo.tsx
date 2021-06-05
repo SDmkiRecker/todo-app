a359f03e187cabdce9c1ae2f221d9e9333685b8e
Sachitha Dharshana <sachitha.dharshana@gmail.com>
2021-06-05 18:20:15 +0530

    Button text change
    Updated the Add task button text to Save

--- src/Components/AddTodo.tsx
+++ src/Components/AddTodo.tsx
@@ -22,7 +22,7 @@ export const AddTodo = ({
             data-attribute-name="Todo" 
         />
         <button className="bg-blue-500 hover:bg-blue-700 focus:outline-none text-white font-bold py-2 px-4 rounded-full" aria-label="Add a todo">
-            Add Task
+            Save
         </button>
     </form>
 )
